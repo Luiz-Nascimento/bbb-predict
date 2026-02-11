961524603a2904709ea3aa42d8f6c177dbae9b76
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2026-02-11 01:12:50 -0300

    chore: configured application.yml, remove ignoring datasource config

--- src/main/java/br/com/luiz/bbbpredict/BbbPredictBotApplication.java
+++ src/main/java/br/com/luiz/bbbpredict/BbbPredictBotApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class BbbPredictBotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BbbPredictBotApplication.class, args);
+		System.out.println("API Inicializada!");
 	}
 
 }
--- src/main/java/br/com/luiz/bbbpredict/dto/contestant/ContestantRequest.java
+++ src/main/java/br/com/luiz/bbbpredict/dto/contestant/ContestantRequest.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotEmpty;
 public record ContestantRequest(
         @NotEmpty(message = "Field name can not be empty")
         String name,
+        @NotEmpty(message = "Field clobTokenId can not be empty")
         String clobTokenId
 ) {
 }
